461f1f77f7ce7af1afd08c231615709a9830b15e
shadow3aaa <shadow3aaa@qq.com>
2023-06-16 16:41:10 +0800

    Sync process

--- fas-rs-bin/src/sensor/mtk_fpsgo/mod.rs
+++ fas-rs-bin/src/sensor/mtk_fpsgo/mod.rs
@@ -3,24 +3,19 @@ mod parse;
 use std::fs;
 use std::path::Path;
 use std::sync::atomic::{AtomicBool, Ordering};
-use std::sync::mpsc::{self, Receiver, SyncSender};
-use std::sync::Arc;
+use std::sync::{Arc, Mutex};
 use std::thread::{self, JoinHandle};
 
 use fas_rs_fw::prelude::*;
 use parse::*;
 
 pub(crate) const FPSGO: &str = "/sys/kernel/fpsgo";
+pub(crate) const BUFFER_CAP: usize = 512;
 
 pub struct MtkFpsGo {
-    // 接收FrameTime
-    frametime_receiver: Receiver<Vec<FrameTime>>,
-    // 指定接收个数
-    frametime_count_sender: SyncSender<u32>,
-    // 接收Fps
-    fps_receiver: Receiver<Vec<Fps>>,
-    // 指定接收时间段
-    fps_time_sender: SyncSender<Duration>,
+    // 缓冲区
+    frametime_buffer: Arc<Mutex<Vec<FrameTime>>>,
+    fps_buffer: Arc<Mutex<Vec<Fps>>>,
     // 控制启停
     thread_handle: (JoinHandle<()>, JoinHandle<()>),
     pause: Arc<AtomicBool>,
@@ -42,41 +37,35 @@ impl VirtualFrameSensor for MtkFpsGo {
         // 控制启停的原子bool
         let pause = Arc::new(AtomicBool::new(false));
 
-        // 管道
-        let (frametime_sender, frametime_receiver) = mpsc::sync_channel(1);
-        let (frametime_count_sender, frametime_count_receiver) = mpsc::sync_channel(1);
-
-        let (fps_sender, fps_receiver) = mpsc::sync_channel(1);
-        let (fps_time_sender, fps_time_receiver) = mpsc::sync_channel(1);
+        // 缓冲区
+        let frametime_buffer = Arc::new(Mutex::new(Vec::with_capacity(BUFFER_CAP)));
+        let fps_buffer = Arc::new(Mutex::new(Vec::with_capacity(BUFFER_CAP)));
 
         let pause_frametime = pause.clone();
         let pause_fps = pause.clone();
 
+        let frametime_clone = frametime_buffer.clone();
+        let fps_clone = fps_buffer.clone();
+
         let thread_handle = (
-            thread::spawn(move || {
-                frametime_thread(frametime_sender, frametime_count_receiver, pause_frametime)
-            }),
-            thread::spawn(move || fps_thread(fps_sender, fps_time_receiver, pause_fps)),
+            thread::spawn(move || frametime_thread(frametime_clone, pause_frametime)),
+            thread::spawn(move || fps_thread(fps_clone, pause_fps)),
         );
 
         Ok(Self {
-            frametime_receiver,
-            frametime_count_sender,
-            fps_receiver,
-            fps_time_sender,
+            frametime_buffer,
+            fps_buffer,
             pause,
             thread_handle,
         })
     }
 
     fn frametimes(&self, count: u32) -> Vec<FrameTime> {
-        self.frametime_count_sender.send(count).unwrap();
-        self.frametime_receiver.recv().unwrap()
+        *self.frametime_buffer.lock().unwrap()
     }
 
     fn fps(&self, time: Duration) -> Vec<Fps> {
-        self.fps_time_sender.send(time).unwrap();
-        self.fps_receiver.recv().unwrap()
+        *self.fps_buffer.lock().unwrap()
     }
 
     fn pause(&self) -> Result<(), Box<dyn Error>> {
--- fas-rs-bin/src/sensor/mtk_fpsgo/parse.rs
+++ fas-rs-bin/src/sensor/mtk_fpsgo/parse.rs
@@ -3,19 +3,13 @@ use std::fs;
 use std::path::Path;
 use std::sync::atomic::{AtomicBool, Ordering};
 use std::sync::mpsc::{Receiver, SyncSender};
-use std::sync::Arc;
+use std::sync::{Arc, Mutex};
 use std::thread;
 
-use super::FPSGO;
+use super::{BUFFER_CAP, FPSGO};
 use fas_rs_fw::prelude::*;
 
-const BUFFER_CAP: usize = 512;
-
-pub fn frametime_thread(
-    frametime_sender: SyncSender<Vec<FrameTime>>,
-    frametime_count_receiver: Receiver<u32>,
-    pause: Arc<AtomicBool>,
-) {
+pub fn frametime_thread(frametime: Arc<Mutex<Vec<FrameTime>>>, pause: Arc<AtomicBool>) {
     let mut buffer = VecDeque::with_capacity(BUFFER_CAP);
     let mut stamps = [0, 0];
 
@@ -28,8 +22,11 @@ pub fn frametime_thread(
             buffer.pop_front();
         }
 
+        // 尝试复制buffer到读取区
+        *frametime.try_lock().unwrap() = buffer.clone().into();
+
         // 获取第一个时间戳
-        if let Some(stamp) = parse_fbt_info() {
+        if let Some(stamp) = parse_frametime() {
             stamps[0] = stamp
         } else {
             continue;
@@ -38,7 +35,7 @@ pub fn frametime_thread(
         // 轮询(sysfs不可用inotify监听)
         // 值变化后保存为第二个时间戳
         loop {
-            if let Some(stamp) = parse_fbt_info() {
+            if let Some(stamp) = parse_frametime() {
                 if stamps[0] < stamp {
                     stamps[1] = stamp;
                     break;
@@ -55,13 +52,7 @@ pub fn frametime_thread(
     }
 }
 
-pub fn fps_thread(
-    fps_sender: SyncSender<Vec<Fps>>,
-    fps_time_receiver: Receiver<Duration>,
-    pause: Arc<AtomicBool>,
-) {
-    todo!()
-    /*
+pub fn fps_thread(fps: Arc<Mutex<Vec<Fps>>>, pause: Arc<AtomicBool>) {
     let buffer = VecDeque::with_capacity(BUFFER_CAP);
 
     loop {
@@ -72,24 +63,41 @@ pub fn fps_thread(
         if buffer.len() > BUFFER_CAP {
             buffer.pop_front();
         }
-
-
     }
-    */
 }
 
-fn parse_fbt_info() -> Option<u64> {
-    /* 解析第8(从0开始)行
-    1(状态)	0		37	19533	0x4c2e00000021	60(屏幕刷新率)	24029340996131(最新帧的vsync时间戳) */
+/* 解析第8(从0开始)行
+1(状态)	0		37	19533	0x4c2e00000021	60(屏幕刷新率)	24029340996131(最新帧的vsync时间戳) */
+fn parse_frametime() -> Option<u64> {
     let fbt_info = fs::read_to_string(Path::new(FPSGO).join("/fbt/fbt_info")).unwrap();
     let mut parse_line = fbt_info.lines().nth(8)?.split_whitespace();
 
     let enabled = parse_line.next()?.trim().parse::<u64>().ok()? == 1;
 
+    // fpsgo未开启
     if !enabled {
         fs::write(Path::new(FPSGO).join("common/fpsgo_enable"), "1").unwrap();
         return None; // 需要重新读取
     }
 
     return parse_line.nth(6)?.trim().parse::<u64>().ok();
+}
+
+/* 解析需跳过第0行和最后3行，提取第三个元素
+tid	bufID		name		currentFPS	targetFPS	FPS_margin	FPS_margin_GPU	FPS_margin_thrs	sbe_state	HWUI
+23480	0x5b9800000038	bin.mt.plus	60		60		0		0		0		0		1
+8606	0x136d0000000d	curitycenter:ui	60		60		0		0		0		0		1
+23480	0x5b9800000037	bin.mt.plus	57		60		0		0		0		0		1
+2756	0x9d900000001	com.miui.home	60		60		0		0		0		0		1
+23480	0x5b9800000036	bin.mt.plus	0		60		0		0		0		0		1
+23480	0x5b9800000035	bin.mt.plus	-1		60		0		0		0		0		1
+8520	0x1d94000001a3	nputmethod.miui	-1		60		0		0		0		0		1
+8520	0x1d94000001a2	nputmethod.miui	-1		60		0		0		0		0		1
+23480	0x5b9800000031	bin.mt.plus	59		60		0		0		0		0		1
+2898	0x9d700000001	ndroid.systemui	-1		60		0		0		0		0		1
+fstb_self_ctrl_fps_enable:1
+fstb_is_cam_active:0
+dfps_ceiling:60 */
+fn parse_fps() -> Option<Fps> {
+    todo!()
 }
