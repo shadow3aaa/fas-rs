654f7fdbae3e7b546ddbc9c9273aa94852b34383
shadow3aaa <shadow3aaa@qq.com>
2023-06-16 20:55:21 +0800

    Test fpsgo sensor

--- fas-rs-bin/src/sensor/mtk_fpsgo/parse.rs
+++ fas-rs-bin/src/sensor/mtk_fpsgo/parse.rs
@@ -28,14 +28,16 @@ pub(super) fn frametime_thread(frametime: Arc<Mutex<Vec<FrameTime>>>, pause: Arc
         *frametime.try_lock().unwrap() = buffer.clone().into();
 
         // 获取第一个时间戳
-        if let Some(stamp) = parse_frametime() {
+        let fbt_info = fs::read_to_string(Path::new(FPSGO).join("fbt/fbt_info")).unwrap();
+        if let Some(stamp) = parse_frametime(&fbt_info) {
             stamps[0] = stamp
         }
 
         // 轮询(sysfs不可用inotify监听)
         // 值变化后保存为第二个时间戳
         loop {
-            if let Some(stamp) = parse_frametime() {
+            let fbt_info = fs::read_to_string(Path::new(FPSGO).join("fbt/fbt_info")).unwrap();
+            if let Some(stamp) = parse_frametime(&fbt_info) {
                 if stamps[0] < stamp {
                     stamps[1] = stamp;
                     break;
@@ -75,7 +77,8 @@ pub(super) fn fps_thread(fps: Arc<Mutex<Vec<(Instant, Fps)>>>, pause: Arc<Atomic
         // 尝试复制buffer到读取区
         *fps.try_lock().unwrap() = buffer.clone().into();
 
-        if let Some(fps) = parse_fps() {
+        let fpsgo_status = fs::read_to_string(Path::new(FPSGO).join("fstb/fpsgo_status")).unwrap();
+        if let Some(fps) = parse_fps(&fpsgo_status) {
             buffer.push_front((Instant::now(), fps));
         } else {
             enable_fpsgo().unwrap();
@@ -86,10 +89,9 @@ pub(super) fn fps_thread(fps: Arc<Mutex<Vec<(Instant, Fps)>>>, pause: Arc<Atomic
     }
 }
 
-/* 解析第8行:
+/* 解析第9行:
 1(状态)	0		37	19533	0x4c2e00000021	60(屏幕刷新率)	24029340996131(最新帧的vsync时间戳) */
-fn parse_frametime() -> Option<u64> {
-    let fbt_info = fs::read_to_string(Path::new(FPSGO).join("fbt/fbt_info")).unwrap();
+fn parse_frametime(fbt_info: &str) -> Option<u64> {
     let mut parse_line = fbt_info.lines().nth(8)?.split_whitespace();
 
     let enabled = parse_line.next()?.trim().parse::<u64>().ok()? == 1;
@@ -99,7 +101,10 @@ fn parse_frametime() -> Option<u64> {
         return None; // 需要重新读取
     }
 
-    return parse_line.nth(6)?.trim().parse::<u64>().ok();
+    /*println!("{}", fbt_info.lines().nth(8)?.trim());
+    println!("{:?}", parse_line.nth(0));*/
+
+    return parse_line.nth(5)?.trim().parse::<u64>().ok();
 }
 
 /* 解析需跳过第0行和最后3行，提取第3个元素
@@ -109,11 +114,10 @@ tid	bufID		name		currentFPS	targetFPS	FPS_margin	FPS_margin_GPU	FPS_margin_thrs	
 fstb_self_ctrl_fps_enable:1
 fstb_is_cam_active:0
 dfps_ceiling:60 */
-fn parse_fps() -> Option<Fps> {
+fn parse_fps(fpsgo_status: &str) -> Option<Fps> {
     use std::cmp::max;
 
     let mut max_fps = None;
-    let fpsgo_status = fs::read_to_string(Path::new(FPSGO).join("fstb/fpsgo_status")).unwrap();
 
     for line in fpsgo_status.lines().skip(1) {
         if line.contains("fstb_self_ctrl_fps_enable")
@@ -124,7 +128,13 @@ fn parse_fps() -> Option<Fps> {
         }
 
         let mut parsed_line = line.split_whitespace();
-        let this_fps = parsed_line.nth(3)?.parse().ok()?;
+        let this_fps = parsed_line.nth(3)?;
+
+        if this_fps == "-1" {
+            continue;
+        }
+
+        let this_fps = this_fps.parse().ok()?;
 
         if let Some(fps) = max_fps {
             max_fps = Some(max(fps, this_fps));
@@ -134,4 +144,37 @@ fn parse_fps() -> Option<Fps> {
     }
 
     max_fps
+}
+
+#[test]
+fn test_parse() {
+    let fbt_info = r"##clus	max	min	
+    3	2	0	
+    
+    clus	num	c	r	
+    0	4	-1	-1
+    1	3	-1	-1
+    2	1	-1	-1
+    enable	idleprefer	max_blc	max_pid	max_bufID	dfps	vsync
+    1	0		13	8606	0x136d00000021	120	15827015268850
+    
+    pid	bufid		perfidx	
+    2898	0x9d700000001	0
+    8606	0x136d00000021	13
+    26994	0x6955000001a7	12##";
+    assert_eq!(parse_frametime(fbt_info), Some(15827015268850));
+
+    let fpsgo_status = r"##tid	bufID		name		currentFPS	targetFPS	FPS_margin	FPS_margin_GPU	FPS_margin_thrs	sbe_state	HWUI
+    26994	0x69550000025d	bin.mt.plus	120		120		0		0		0		0		1
+    26994	0x69550000025c	bin.mt.plus	115		120		0		0		0		0		1
+    8606	0x136d0000003f	curitycenter:ui	120		120		0		0		0		0		1
+    2756	0x9d9000002ce	com.miui.home	0		120		0		0		0		0		1
+    2898	0x9d700000001	ndroid.systemui	-1		10		0		0		0		0		1
+    24678	0x12fb00000009	m.omarea.vtools	0		10		0		0		0		0		1
+    24678	0x12fb0000000b	m.omarea.vtools	0		10		0		0		0		0		1
+    fstb_self_ctrl_fps_enable:1
+    fstb_is_cam_active:0
+    dfps_ceiling:120##";
+
+    assert_eq!(parse_fps(fpsgo_status), Some(120));
 }
