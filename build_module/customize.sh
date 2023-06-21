3ad067d636441d615b94b7fae44b795decda59ca
shadow3aaa <shadow3aaa@qq.com>
2023-06-21 22:05:18 +0800

    games.txt

--- build_module/customize.sh
+++ build_module/customize.sh
@@ -4,3 +4,11 @@ chmod a+x $MODPATH/fas-rs
 if [ ! $($MODPATH/fas-rs test) == "Supported" ]; then
 	abort
 fi
+
+conf=/sdcard/Android/fas-rs/games.txt
+if [ ! -f $conf ]; then
+    mkdir -p /sdcard/Android/fas-rs
+    cp $MODPATH/games.txt $conf
+fi
+
+rm $MODPATH/games.txt
