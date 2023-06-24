774d2ad21dcbea908c062bd469c79fc9574ce28f
shadow3aaa <shadow3aaa@qq.com>
2023-06-24 18:57:08 +0800

    Opt cpu controll

--- src/controller/cpu_common.rs
+++ src/controller/cpu_common.rs
@@ -3,12 +3,15 @@ use bimap::BiHashMap;
 use fas_rs_fw::prelude::*;
 
 use std::{
-    cell::RefCell,
+    cell::{Cell, RefCell},
     fs,
     path::{Path, PathBuf},
 };
 
-const CPUFREQ: &str = "/sys/devices/system/cpu/cpufreq";
+const POLICY_PATH: &str = "/sys/devices/system/cpu/cpufreq";
+// 升频: 向频率上限写入`当前频率pos + BALANCE + 升频次数`的频率
+// 降频: 向频率上限写入`当前频率pos + BALANCE - 降频次数`的频率
+const JUMP_BALANCE: usize = 3;
 
 type Frequency = usize;
 type FrequencyTable = BiHashMap<usize, Frequency>;
@@ -16,6 +19,7 @@ type Policy = (PathBuf, FrequencyTable);
 pub struct CpuCommon {
     write_pool: RefCell<WritePool>,
     policys: Vec<Policy>,
+    jump: Cell<isize>,
 }
 
 impl CpuCommon {
@@ -40,7 +44,7 @@ impl VirtualPerformanceController for CpuCommon {
     where
         Self: Sized,
     {
-        Path::new(CPUFREQ).exists()
+        Path::new(POLICY_PATH).exists()
     }
 
     fn new() -> Result<Self, Box<dyn Error>>
@@ -49,7 +53,7 @@ impl VirtualPerformanceController for CpuCommon {
     {
         let mut policys = Vec::with_capacity(5);
 
-        let cpufreq = fs::read_dir(CPUFREQ)?;
+        let cpufreq = fs::read_dir(POLICY_PATH)?;
         for policy in cpufreq {
             let path = policy?.path();
             let mut freq_table: Vec<Frequency> =
@@ -69,8 +73,8 @@ impl VirtualPerformanceController for CpuCommon {
             let name_a = a.0.file_name().unwrap().to_str().unwrap();
             let name_b = b.0.file_name().unwrap().to_str().unwrap();
 
-            let num_a: usize = name_a.split("policy").nth(1).unwrap().parse().unwrap();
-            let num_b: usize = name_b.split("policy").nth(1).unwrap().parse().unwrap();
+            let num_a: u32 = name_a.split("policy").nth(1).unwrap().parse().unwrap();
+            let num_b: u32 = name_b.split("policy").nth(1).unwrap().parse().unwrap();
             num_b.cmp(&num_a)
         });
         policys.truncate(2); // 保留后两个集群即可
@@ -78,45 +82,64 @@ impl VirtualPerformanceController for CpuCommon {
         Ok(Self {
             write_pool: RefCell::new(WritePool::new(4)),
             policys,
+            jump: Cell::new(0),
         })
     }
 
     fn limit(&self) {
+        if self.jump.get() > 0 {
+            self.jump.set(0);
+        }
+
         self.policys.iter().for_each(|policy| {
             let (pos, _) = Self::read_cur_freq(policy);
             let (path, table) = policy;
 
-            let pos = pos.saturating_sub(1);
-            if let Some(freq) = table.get_by_left(&pos) {
-                self.write_pool
-                    .borrow_mut()
-                    .write(&path.join("scaling_max_freq"), &freq.to_string())
-                    .unwrap();
-            }
-        })
+            let pos = (pos + JUMP_BALANCE).saturating_sub(self.jump.get().wrapping_abs() as usize);
+            let write_freq = table
+                .get_by_left(&pos)
+                .unwrap_or_else(|| table.right_values().max().unwrap())
+                .to_string();
+
+            self.write_pool
+                .borrow_mut()
+                .write(&path.join("scaling_max_freq"), &write_freq)
+                .unwrap();
+        });
+
+        self.jump.set(self.jump.get() - 1);
     }
 
     fn release(&self) {
+        if self.jump.get() < 0 {
+            self.jump.set(0);
+        }
+
         self.policys.iter().for_each(|policy| {
             let (pos, _) = Self::read_cur_freq(policy);
             let (path, table) = policy;
 
-            let pos = pos + 1;
-            if let Some(freq) = table.get_by_left(&pos) {
-                self.write_pool
-                    .borrow_mut()
-                    .write(&path.join("scaling_max_freq"), &freq.to_string())
-                    .unwrap();
-            }
-        })
+            let pos = pos + JUMP_BALANCE + self.jump.get() as usize;
+            let write_freq = table
+                .get_by_left(&pos)
+                .unwrap_or_else(|| table.right_values().max().unwrap())
+                .to_string();
+
+            self.write_pool
+                .borrow_mut()
+                .write(&path.join("scaling_max_freq"), &write_freq)
+                .unwrap();
+        });
+
+        self.jump.set(self.jump.get() + 1);
     }
 
     fn plug_in(&self) -> Result<(), Box<dyn Error>> {
         self.policys.iter().for_each(|policy| {
-            let max_freq = Self::max_freq(policy).unwrap();
+            let max_freq = Self::max_freq(policy).unwrap().to_string();
             self.write_pool
                 .borrow_mut()
-                .write(&policy.0.join("scaling_max_freq"), &max_freq.to_string())
+                .write(&policy.0.join("scaling_max_freq"), &max_freq)
                 .unwrap();
         });
         Ok(())
