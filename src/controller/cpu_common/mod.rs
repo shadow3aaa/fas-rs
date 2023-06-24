4e53e83be9f8d4c5b99d4e1f6bf8c4b81bb11e41
shadow3aaa <1394025007@qq.com>
2023-06-24 12:43:59 +0800

    modified:   Cargo.lock modified:   Cargo.toml modified:   src/controller/cpu_common/mod.rs deleted:    src/controller/cpu_common/process.rs modified:   src/controller/mod.rs renamed:    src/controller/cpu_common/write_pool.rs -> src/controller/write_pool.rs

--- src/controller/cpu_common/mod.rs
+++ src/controller/cpu_common/mod.rs
@@ -1,35 +1,37 @@
-mod process;
-pub(crate) mod write_pool;
-
+use super::write_pool::WritePool;
+use bimap::BiHashMap;
 use fas_rs_fw::prelude::*;
-use std::{
-    fs,
-    path::Path,
-    sync::mpsc::{self, Sender},
-    thread::{self, JoinHandle},
-};
 
-use process::*;
+use std::{
+    cell::RefCell,
+    fs,
+    path::{Path, PathBuf},
+};
 
 const CPUFREQ: &str = "/sys/devices/system/cpu/cpufreq";
 
-pub(crate) type Frequency = usize;
-pub(crate) type FrequencyTable = Vec<Frequency>;
+type Frequency = usize;
+type FrequencyTable = BiHashMap<usize, Frequency>;
+type Policy = (PathBuf, FrequencyTable);
 pub struct CpuCommon {
-    command_sender: Sender<Command>,
-    thread_handle: JoinHandle<()>,
+    write_pool: RefCell<WritePool>,
+    policys: Vec<Policy>,
 }
 
-pub(crate) enum Command {
-    Stop,
-    Release,
-    Limit,
-    Pause,
-}
+impl CpuCommon {
+    fn read_cur_freq(policy: &Policy) -> (usize, Frequency) {
+        let freq = fs::read_to_string(policy.0.join("scaling_cur_freq"))
+            .unwrap()
+            .trim()
+            .parse::<usize>()
+            .unwrap();
+        let pos = policy.1.get_by_right(&freq).unwrap();
+        (*pos, freq)
+    }
 
-impl Drop for CpuCommon {
-    fn drop(&mut self) {
-        let _ = self.command_sender.send(Command::Stop);
+    fn max_freq(policy: &Policy) -> Result<Frequency, Box<dyn Error>> {
+        let table = &policy.1;
+        Ok(*table.right_values().max().ok_or("None")?)
     }
 }
 
@@ -45,56 +47,89 @@ impl VirtualPerformanceController for CpuCommon {
     where
         Self: Sized,
     {
-        let mut table = Vec::with_capacity(2);
+        let mut policys = Vec::with_capacity(5);
 
         let cpufreq = fs::read_dir(CPUFREQ)?;
         for policy in cpufreq {
             let path = policy?.path();
-            let table_path = path.join("scaling_available_frequencies");
-            let this_table: FrequencyTable = fs::read_to_string(table_path)?
-                .split_whitespace()
-                .filter_map(|freq| freq.parse().ok())
-                .collect();
+            let mut freq_table: Vec<Frequency> =
+                fs::read_to_string(path.join("scaling_available_frequencies"))?
+                    .split_whitespace()
+                    .filter_map(|freq| freq.parse().ok())
+                    .collect();
+            freq_table.sort_unstable();
+            let freq_table: BiHashMap<usize, Frequency> =
+                freq_table.into_iter().enumerate().collect();
 
-            table.push((this_table, path.join("scaling_max_freq")));
+            policys.push((path, freq_table));
         }
 
         // 按policy降序排列
-        table.sort_by(|a, b| {
-            let name_a = a.1.parent().unwrap().file_name().unwrap().to_str().unwrap();
-            let name_b = b.1.parent().unwrap().file_name().unwrap().to_str().unwrap();
+        policys.sort_by(|a, b| {
+            let name_a = a.0.file_name().unwrap().to_str().unwrap();
+            let name_b = b.0.file_name().unwrap().to_str().unwrap();
 
             let num_a: usize = name_a.split("policy").nth(1).unwrap().parse().unwrap();
             let num_b: usize = name_b.split("policy").nth(1).unwrap().parse().unwrap();
             num_b.cmp(&num_a)
         });
-        table.truncate(2); // 保留后两个集群即可
-
-        let (command_sender, command_receiver) = mpsc::channel();
-
-        let thread_handle = thread::spawn(move || process_freq(table, command_receiver));
+        policys.truncate(2); // 保留后两个集群即可
 
         Ok(Self {
-            command_sender,
-            thread_handle,
+            write_pool: RefCell::new(WritePool::new(4)),
+            policys,
         })
     }
 
     fn limit(&self) {
-        self.command_sender.send(Command::Limit).unwrap();
+        self.policys.iter().for_each(|policy| {
+            let (pos, _) = Self::read_cur_freq(policy);
+            let (path, table) = policy;
+
+            let pos = pos.saturating_sub(1);
+            if let Some(freq) = table.get_by_left(&pos) {
+                self.write_pool
+                    .borrow_mut()
+                    .write(&path.join("scaling_max_freq"), &freq.to_string())
+                    .unwrap();
+            }
+        })
     }
 
     fn release(&self) {
-        self.command_sender.send(Command::Release).unwrap();
+        self.policys.iter().for_each(|policy| {
+            let (pos, _) = Self::read_cur_freq(policy);
+            let (path, table) = policy;
+
+            let pos = pos + 1;
+            if let Some(freq) = table.get_by_left(&pos) {
+                self.write_pool
+                    .borrow_mut()
+                    .write(&path.join("scaling_max_freq"), &freq.to_string())
+                    .unwrap();
+            }
+        })
     }
 
     fn plug_in(&self) -> Result<(), Box<dyn Error>> {
-        self.thread_handle.thread().unpark();
+        self.policys.iter().for_each(|policy| {
+            let max_freq = Self::max_freq(policy).unwrap();
+            self.write_pool
+                .borrow_mut()
+                .write(&policy.0.join("scaling_max_freq"), &max_freq.to_string())
+                .unwrap();
+        });
         Ok(())
     }
 
     fn plug_out(&self) -> Result<(), Box<dyn Error>> {
-        self.command_sender.send(Command::Pause).unwrap();
+        self.policys.iter().for_each(|policy| {
+            let max_freq = Self::max_freq(policy).unwrap();
+            self.write_pool
+                .borrow_mut()
+                .write(&policy.0.join("scaling_max_freq"), &max_freq.to_string())
+                .unwrap();
+        });
         Ok(())
     }
 }
--- src/controller/mod.rs
+++ src/controller/mod.rs
@@ -1,3 +1,4 @@
 //! 放置[`fas_rs_fw::VirtualPerformanceController`]的实现
 
 pub mod cpu_common;
+pub(crate) mod write_pool;
