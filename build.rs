c27ac85734c87858f0225652fe4d42ce7fd10067
shadow3aaa <shadow3aaa@qq.com>
2023-07-18 15:50:09 +0800

    add authors

--- build.rs
+++ build.rs
@@ -4,6 +4,7 @@ use serde_derive::Deserialize;
 
 #[derive(Deserialize, Debug)]
 struct Package {
+    pub authors: Vec<String>,
     pub name: String,
     pub version: String,
     pub description: String,
@@ -24,6 +25,12 @@ fn main() -> Result<(), Box<dyn Error>> {
     let package = data.package;
     let id = package.name.replace('-', "_"); // 符合magisk module id要求
     let version_code: usize = package.version.replace('.', "").trim().parse()?; // 转为纯数字版本
+    let authors = package.authors;
+    let mut author = String::new();
+    for a in authors {
+        author = format!("{author}{a} ");
+    }
+    let author = author.trim();
 
     let mut file = fs::OpenOptions::new()
         .create(true)
@@ -35,6 +42,7 @@ fn main() -> Result<(), Box<dyn Error>> {
     writeln!(file, "name={}", package.name)?;
     writeln!(file, "version=v{}", package.version)?;
     writeln!(file, "versionCode={version_code}")?;
+    writeln!(file, "author={author}")?;
     writeln!(
         file,
         "description={} {}",
