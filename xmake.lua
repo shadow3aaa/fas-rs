3db7b4a018746fecefce06fb695ec593c4e44cb3
shadow3aaa <1394025007@qq.com>
2023-10-29 16:53:40 +0800

    README

--- xmake.lua
+++ xmake.lua
@@ -2,42 +2,42 @@ add_rules("mode.release", "mode.debug")
 includes("zygisk")
 
 target("fas-rs")
-    on_clean(function (target)
-        os.exec("cargo clean")
-        os.rmdir("output")
-    end)
-    
-    before_build(function (target)
-        os.rm("output/.temp")
-        os.cp("module", "output/.temp")
-    end)
+on_clean(function(target)
+    os.exec("cargo clean")
+    os.rmdir("output")
+end)
 
-    if is_mode("debug") then
-        on_build(function (target)
-            os.exec("cargo build --target aarch64-linux-android")
-        end)
-    else
-        on_build(function (target)
-            os.exec("cargo build --release --target aarch64-linux-android")
-        end)
-    end
+before_build(function(target)
+    os.rm("output/.temp")
+    os.cp("module", "output/.temp")
+end)
+
+if is_mode("debug") then
+    on_build(function(target)
+        os.exec("cargo build --target aarch64-linux-android")
+    end)
+else
+    on_build(function(target)
+        os.exec("cargo build --release --target aarch64-linux-android")
+    end)
+end
 
 target("package")
-    add_deps("fas-rs")
-    add_deps("zygisk")
+add_deps("fas-rs")
+add_deps("zygisk")
 
-    on_build(function (target)
-        local temp = path.join(target:scriptdir(), "output/.temp")
-        
-        if is_mode("debug") then
-            os.cp("target/aarch64-linux-android/debug/fas-rs", "output/.temp/fas-rs")
-        else
-            os.cp("target/aarch64-linux-android/release/fas-rs", "output/.temp/fas-rs")
-        end
+on_build(function(target)
+    local temp = path.join(target:scriptdir(), "output/.temp")
 
-        os.cp("zygisk/output/arm64-v8a.so", "output/.temp/zygisk/arm64-v8a.so")
-        
-        os.cd(temp)
-        os.exec("zip -9 -rq ../fas-rs.zip .")
-        print("Flashable Module Packaged: output/fas-rs.zip")
-    end)
+    if is_mode("debug") then
+        os.cp("target/aarch64-linux-android/debug/fas-rs", "output/.temp/fas-rs")
+    else
+        os.cp("target/aarch64-linux-android/release/fas-rs", "output/.temp/fas-rs")
+    end
+
+    os.cp("zygisk/output/arm64-v8a.so", "output/.temp/zygisk/arm64-v8a.so")
+
+    os.cd(temp)
+    os.exec("zip -9 -rq ../fas-rs.zip .")
+    print("Flashable Module Packaged: output/fas-rs.zip")
+end)
