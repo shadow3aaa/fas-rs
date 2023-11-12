97e3b03acc07dac7387b8e57f734b340764477d1
shadow3aaa <shadow3aaa@qq.com>
2023-11-13 06:58:09 +0800

    手动调用clang

--- zygisk/build.sh
+++ zygisk/build.sh
@@ -14,9 +14,23 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 SHDIR="$(dirname $(readlink -f "$0"))"
+CFLAGS="
+-O3 -flto -fmerge-all-constants -fno-exceptions -fomit-frame-pointer -fshort-enums
+-Wl,-O3,--lto-O3,--gc-sections,--as-needed,--icf=all,-z,norelro,--pack-dyn-relocs=android+relr
+-std=c++2b -Wall -lc++"
 
 if [ "$TERMUX_VERSION" = "" ]; then
-    alias cargo='cargo ndk -t arm64-v8a'
+	alias cargo='cargo ndk -t arm64-v8a'
+
+	if [ "$ANDROID_NDK_HOME" = "" ]; then
+		echo Missing ANDROID_NDK_HOME
+		exit 1
+	else
+		local dir=$ANDROID_NDK_HOME/toolchains/llvm/prebuilt/linux-x86_64/bin
+		local clang_latest=$(ls $dir | grep aarch64-linux-android | grep clang | tail -n 1)
+
+		alias clang++="$dir/$clang_latest"
+	fi
 fi
 
 mkdir -p $SHDIR/output
@@ -25,38 +39,44 @@ set -e
 
 case $1 in
 clean | --clean)
-    cd $SHDIR/rust
-    cargo clean
+	cd $SHDIR/rust
+	cargo clean
 
-    cd $SHDIR
-    rm -rf output
+	cd $SHDIR
+	rm -rf output
 
-    exit
-    ;;
+	exit
+	;;
 r | -r | release | --release)
-    cd $SHDIR/rust
-    cargo build --release
+	cd $SHDIR/rust
+	cargo build --release
 
-    cd $SHDIR
-    cp -f rust/target/aarch64-linux-android/release/librust.a output/librust.a
+	cd $SHDIR
+	cp -f rust/target/aarch64-linux-android/release/librust.a output/librust.a
 
-    if [ "$TERMUX_VERSION" = "" ]; then
-        xmake f -p android -a arm64-v8a -m release
-    fi
+	clang++ --shared src/*.cpp \
+		-I rust/include \
+		-L output -L ../prebuilt \
+		-fPIC -nostdlib++ -Wl,-lrust,-llog,-lbinder_ndk \
+		$CFLAGS \
+		-o output/arm64-v8a.so
 
-    xmake
-    ;;
+	strip output/arm64-v8a.so
+	;;
 d | -d | debug | --debug)
-    cd $SHDIR/rust
-    cargo build
+	cd $SHDIR/rust
+	cargo build
 
-    cd $SHDIR
-    cp -f rust/target/aarch64-linux-android/debug/librust.a output/librust.a
+	cd $SHDIR
+	cp -f rust/target/aarch64-linux-android/debug/librust.a output/librust.a
 
-    if [ "$TERMUX_VERSION" = "" ]; then
-        xmake f -p android -a arm64-v8a -m debug
-    fi
+	clang++ --shared src/*.cpp \
+		-I rust/include \
+		-L output -L ../prebuilt \
+		-fPIC -nostdlib++ -Wl,-lrust,-llog,-lbinder_ndk \
+		$CFLAGS \
+		-o output/arm64-v8a.so
 
-    xmake
-    ;;
+	strip output/arm64-v8a.so
+	;;
 esac
