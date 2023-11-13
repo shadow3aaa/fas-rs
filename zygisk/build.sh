5cd459f60bcb1a188fe776ae9aa9888b0559f720
shadow3aaa <shadow3aaa@qq.com>
2023-11-13 15:14:01 +0800

    cla

--- zygisk/build.sh
+++ zygisk/build.sh
@@ -29,6 +29,7 @@ if [ "$TERMUX_VERSION" = "" ]; then
 		dir=$ANDROID_NDK_HOME/toolchains/llvm/prebuilt/linux-x86_64/bin
 		clang_latest=$(ls $dir | grep aarch64-linux-android | grep clang | tail -n 1)
 
+        echo Find clang: $dir/$clang_latest
 		alias clang++="$dir/$clang_latest"
 		echo Use clang: $(which clang++)
 	fi
