8cf1b202e8e00cdeb011dc236bdcd0ef7476bd16
shadow3aaa <shadow3aaa@qq.com>
2024-02-09 15:45:40 +0800

    修复readme安装残留

--- module/customize.sh
+++ module/customize.sh
@@ -75,8 +75,8 @@ else
 	cp $MODPATH/games.toml $CONF
 fi
 
-cp -f $MODPATH/README.md $DIR/doc_cn.md
-cp -f $MODPATH/README_EN.md $DIR/doc_en.md
+mv -f $MODPATH/README.md $DIR/doc_cn.md
+mv -f $MODPATH/README_EN.md $DIR/doc_en.md
 
 sh $MODPATH/vtools/init_vtools.sh $(realpath $MODPATH/module.prop)
 
