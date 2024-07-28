347dad37acec387e14ebf1d241a70cc35b5d811e
shadow3aaa <shadow3aaa@qq.com>
2024-07-28 15:49:13 +0800

    refactor: 删除无用的clamp

--- src/cpu_common/mod.rs
+++ src/cpu_common/mod.rs
@@ -123,8 +123,7 @@ impl Controller {
     pub fn fas_update_freq(&mut self, factor: f64, config: &Config) {
         self.policy_freq = self
             .policy_freq
-            .saturating_add((BASE_FREQ as f64 * factor) as isize)
-            .clamp(self.min_freq, self.max_freq);
+            .saturating_add((BASE_FREQ as f64 * factor) as isize);
 
         for cpu in &self.cpu_infos {
             cpu.write_freq(self.policy_freq, &mut self.file_handler, config)
