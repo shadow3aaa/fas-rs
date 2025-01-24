0c94b8bb5e7bf73133b92d1979f242511f3e2ced
shadow3aaa <shadow3aaaa@gmail.com>
2025-01-24 14:26:47 +0800

    refactor: 调整目标FPS偏移的CPU利用率阈值

--- src/framework/scheduler/looper/policy/controll.rs
+++ src/framework/scheduler/looper/policy/controll.rs
@@ -79,9 +79,9 @@ fn adjust_target_fps(target_fps: f64, controller_state: &mut ControllerState) ->
         controller_state.usage_sample_timer = Instant::now();
         let util = controller_state.controller.util_max();
 
-        if util <= 0.25 {
+        if util <= 0.5 {
             controller_state.target_fps_offset -= 0.1;
-        } else if util >= 0.3 {
+        } else if util >= 0.6 {
             controller_state.target_fps_offset += 0.1;
         }
     }
