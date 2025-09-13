8f401f73e2a7fe8e460f7f879dc228f0e17aa201
Tools-app <localhost.hutao@gmail.com>
2025-09-13 15:37:30 +0800

    ci: readd telegram bot
    Signed-off-by: Tools-app <localhost.hutao@gmail.com>

---
+++ .github/bot.py
@@ -0,0 +1,43 @@
+from telethon import TelegramClient
+import asyncio
+from telethon.sessions import StringSession
+import os
+
+API_ID = 611335
+API_HASH = "d524b414d21f4d37f08684c1df41ac9c"
+
+BOT_TOKEN = os.environ.get("BOT_TOKEN")
+CHAT_ID = int(os.environ.get("CHAT_ID"))
+COMMIT_MESSAGE = os.environ.get("COMMIT_MESSAGE")
+BOT_CI_SESSION = os.environ.get("BOT_CI_SESSION")
+MSG_TEMPLATE = """
+```
+{commit_message}
+```
+""".strip()
+
+
+def get_caption():
+    msg = MSG_TEMPLATE.format(
+        commit_message=COMMIT_MESSAGE,
+    )
+    if len(msg) > 1024:
+        return COMMIT_URL
+    return msg
+
+async def send_telegram_message():
+    async with TelegramClient(StringSession(BOT_CI_SESSION), api_id=API_ID, api_hash=API_HASH) as client:
+        await client.start(bot_token=BOT_TOKEN)
+        print("[+] Caption: ")
+        print("---")
+        print("---")
+        print("[+] Sending")
+        await client.send_file(
+            entity=CHAT_ID,
+            file='./output/fas-rs-next(release).zip',
+            caption=get_caption(),
+            parse_mode="markdown"
+        )
+
+if __name__ == '__main__':
+    asyncio.run(send_telegram_message())
